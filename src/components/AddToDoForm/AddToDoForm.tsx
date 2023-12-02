7b347008abcb6391f27249626e74f665580e7aa4
Robert Chapman <chapmanrobert12@gmail.com>
2023-12-02 15:12:43 -0600

    Installed react hook form dependency for submitting todos. The AddToDoForm component can now be used to add tasks to the todo list

--- src/components/AddToDoForm/AddToDoForm.tsx
+++ src/components/AddToDoForm/AddToDoForm.tsx
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { ToDoContext } from "../../context/todo-context";
+
 export default function AddToDoForm() {
+  
+  const { register, handleSubmit } = useForm();
+
+  const { addToDo } = useContext(ToDoContext);
+  
+  const onSubmit = (data: FieldValues) => {
+    addToDo(data.todoText);
+  }
+
   return (
-    <form className="w-full flex justify-center">
+    <form className="w-full flex justify-center" onSubmit={handleSubmit(onSubmit)}>
       <input
+        {...register("todoText", {required: true})}
         type="text"
         placeholder="e.g. go shopping"
         className="w-3/4 h-14 text-xl text-white rounded-xl pl-3 bg-secondary-900 outline-none focus:outline-none focus:bg-accent-600 focus:ring-4 focus:ring-accent-800 focus:placeholder:text-gray-600 transition-all"
