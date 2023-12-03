df1f9652eb10c0c0610a32a1c206b3dfa6b87b1e
Robert Chapman <chapmanrobert12@gmail.com>
2023-12-03 13:31:39 -0600

    Touched up some of the styling

--- src/components/AddToDoForm/AddToDoForm.tsx
+++ src/components/AddToDoForm/AddToDoForm.tsx
@@ -18,7 +18,7 @@ export default function AddToDoForm() {
         {...register("todoText", {required: true})}
         type="text"
         placeholder="e.g. go shopping"
-        className="w-3/4 h-14 text-xl text-white rounded-xl pl-3 bg-secondary-900 outline-none focus:outline-none focus:bg-accent-600 focus:ring-4 focus:ring-accent-800 focus:placeholder:text-gray-600 transition-all"
+        className="w-full h-14 text-xl text-white rounded-xl pl-3 bg-secondary-900 outline-none focus:outline-none focus:bg-accent-600 focus:ring-4 focus:ring-accent-800 focus:placeholder:text-gray-600 transition-all"
       />
     </form>
   );
