7db6267e093a1a94b291d2c8989a3e14808760e7
Robert Chapman <chapmanrobert12@gmail.com>
2023-12-03 08:00:05 -0600

    Stylized the todo item component more to start implementing the UI

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -5,5 +5,9 @@ interface ToDoItemProps {
 }
 
 export default function ToDoItem({ todo }: ToDoItemProps) {
-  return <h1>{todo.text}</h1>;
+  return (
+    <div className="w-full h-14 px-2 flex items-center rounded-xl bg-secondary-900">
+      <h1 className="text-xl ml-2 text-white">{todo.text}</h1>
+    </div>
+  );
 }
