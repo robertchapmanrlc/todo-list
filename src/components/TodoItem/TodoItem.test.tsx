70b4a2d77d95ebcc0b8c5abecfc15731d4e9106f
Robert Chapman <chapmanrobert12@gmail.com>
2023-12-03 09:52:15 -0600

    ToDo item component will enter edit mode when clicking the pencil icon. Unit test ensure correct elements are rendered in edit mode

--- src/components/TodoItem/TodoItem.test.tsx
+++ src/components/TodoItem/TodoItem.test.tsx
@@ -1,5 +1,5 @@
 import ToDoItem from "./TodoItem";
-import { render, screen } from "../../utils/test-utils";
+import { render, screen, userEvent } from "../../utils/test-utils";
 import { Todo } from "../../utils/types";
 
 const todo: Todo = {
@@ -25,6 +25,35 @@ describe("ToDoItem", () => {
       const { container } = render(<ToDoItem todo={todo} />);
       const trashSvg: SVGElement = container.querySelector('.lucide-trash') as SVGElement;
       expect(trashSvg).toBeVisible();
+    });
+  });
+
+  describe("behavior", () => {
+    test("shows textbox in edit mode", async () => {
+      render(<ToDoItem todo={todo} />);
+      const editButton: HTMLButtonElement = screen.getAllByRole('button')[0] as HTMLButtonElement;
+      await userEvent.click(editButton);
+
+      const input: HTMLInputElement = screen.getByRole('textbox');
+      expect(input).toBeVisible();
+    });
+
+    test("shows confirm icon in edit mode", async () => {
+      const { container } = render(<ToDoItem todo={todo} />);
+      const editButton: HTMLButtonElement = screen.getAllByRole('button')[0] as HTMLButtonElement;
+      await userEvent.click(editButton);
+
+      const confirmSvg: SVGElement = container.querySelector('.lucide-check-circle2') as SVGElement;
+      expect(confirmSvg).toBeVisible();
+    });
+
+    test("shows cancel icon in edit mode", async () => {
+      const { container } = render(<ToDoItem todo={todo} />);
+      const editButton: HTMLButtonElement = screen.getAllByRole('button')[0] as HTMLButtonElement;
+      await userEvent.click(editButton);
+
+      const cancelSvg: SVGElement = container.querySelector('.lucide-xcircle') as SVGElement;
+      expect(cancelSvg).toBeVisible();
     });
   });
 });
--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,32 +1,62 @@
-import { Pencil, Trash } from "lucide-react";
+import { CheckCircle2, Pencil, Trash, XCircle } from "lucide-react";
 
 import { Todo } from "../../utils/types";
 import { useToDoContext } from "../../context/todo-context";
+import { useState } from "react";
 
 interface ToDoItemProps {
   todo: Todo;
 }
 
 export default function ToDoItem({ todo }: ToDoItemProps) {
+  const [editing, setEditing] = useState(false);
+
   const { deleteToDo } = useToDoContext();
 
   return (
-    <div className="w-full h-14 px-4 flex items-center justify-between rounded-xl bg-secondary-900">
-      <h1 className="text-xl text-white">{todo.text}</h1>
-      <div className="w-16 flex justify-between">
-        <button
-          className="hover:scale-125 group transition-all"
-          onClick={() => deleteToDo(todo.id)}
-        >
-          <Pencil className="text-accent-600 group-hover:text-accent-300 transition-all" />
-        </button>
-        <button
-          className="hover:scale-125 group transition-all"
-          onClick={() => deleteToDo(todo.id)}
-        >
-          <Trash className="text-accent-600 group-hover:text-accent-300 transition-all" />
-        </button>
-      </div>
-    </div>
+    <>
+      {!editing ? (
+        <div className="w-full h-14 px-4 flex items-center justify-between rounded-xl bg-secondary-900">
+          <h1 className="text-xl text-white">{todo.text}</h1>
+          <div className="w-16 flex justify-between">
+            <button
+              className="hover:scale-125 group transition-all"
+              onClick={() => setEditing(true)}
+            >
+              <Pencil className="text-accent-600 group-hover:text-accent-300 transition-all" />
+            </button>
+            <button
+              className="hover:scale-125 group transition-all"
+              onClick={() => deleteToDo(todo.id)}
+            >
+              <Trash className="text-accent-600 group-hover:text-accent-300 transition-all" />
+            </button>
+          </div>
+        </div>
+      ) : (
+        <form className="w-full h-14 px-4 flex items-center justify-between rounded-xl bg-accent-600">
+          <input
+            type="text"
+            defaultValue={todo.text}
+            className="w-3/4 bg-transparent text-xl text-white py-1 outline-none border-b-2 border-transparent focus:border-b-2 focus:border-b-white focus:outline-none transition-all"
+            autoFocus
+          />
+          <div className="w-16 flex justify-between">
+            <button
+              className="hover:scale-125 transition-all group"
+              type="submit"
+            >
+              <CheckCircle2 className="text-white/75 group-hover:text-white" />
+            </button>
+            <button
+              className="ms-2 hover:scale-125 transition-all group"
+              onClick={() => setEditing(false)}
+            >
+              <XCircle className="text-white/75 group-hover:text-white transition-all" />
+            </button>
+          </div>
+        </form>
+      )}
+    </>
   );
 }
